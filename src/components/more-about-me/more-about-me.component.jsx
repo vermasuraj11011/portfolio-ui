8964ebb986b1a97ed49e6e67178df4ae1944f718
Suraj Verma <vermasuraj008@gmail.com>
2026-06-29 22:36:36 +0530

    adding more about

---
+++ src/components/more-about-me/more-about-me.component.jsx
@@ -0,0 +1,11 @@
+import "./more-about-me.styles.scss";
+
+const MoreAboutMe = () => {
+  return (
+    <div className="more-about-me">
+      <div className="more-about-me-container">More About Me</div>
+    </div>
+  );
+};
+
+export default MoreAboutMe;
